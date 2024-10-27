4bc1fbc84253b4c6db0f944511b51b9bd6ae6f87
ammuiyer <ammu.iyer5@gmail.com>
2024-10-27 17:19:42 -0400

    trying again

--- generate.py
+++ generate.py
@@ -1,12 +1,14 @@
-from transformers import AutoModelForCausalLM
- 
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
-model.to("cuda")
- 
-generated_ids = model.generate(tokens, max_new_tokens=1000, do_sample=True)
+from huggingface_hub import login
+import os
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-# decode with mistral tokenizer
-result = tokenizer.decode(generated_ids[0].tolist())
-print(result)
+model_id = "mistralai/Mixtral-8x22B-v0.1"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
 
+model = AutoModelForCausalLM.from_pretrained(model_id)
 
+text = "Hello my name is"
+inputs = tokenizer(text, return_tensors="pt")
+
+outputs = model.generate(**inputs, max_new_tokens=20)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
